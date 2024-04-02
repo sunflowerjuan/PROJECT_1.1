d8de49d0b127cf300529ddca61a7ffa670f58f3d
Sebastian Barajas <sebasjuan331@gmail.com>
2024-04-02 09:10:28 -0500

    se agrego Place Services

--- src/main/java/co/edu/uptc/project_1/services/PlaceServices.java
+++ src/main/java/co/edu/uptc/project_1/services/PlaceServices.java
@@ -1,8 +1,63 @@
 package co.edu.uptc.project_1.services;
 
-import co.edu.uptc.services.FileManager.FileManager;
+import java.util.List;
+
+import co.edu.uptc.project_1.exceptions.ProjectExeption;
+import co.edu.uptc.project_1.exceptions.TypeMessage;
+import co.edu.uptc.project_1.model.Place;
+import co.edu.uptc.services.myList.SimpleList;
 
 public class PlaceServices {
-    private String path = "places.txt";
-    private FileManager fileManager = new FileManager(path);
+    private List<Place> placesList = new SimpleList<>();
+
+    public void add(Place place) throws ProjectExeption {
+        try {
+            placesList.add(place);
+        } catch (Exception e) {
+            throw new ProjectExeption(TypeMessage.NOT_FOUND_FILE);
+        }
+
+    }
+
+    public List<Place> getPlaces() throws ProjectExeption {
+        return placesList;
+    }
+
+    public void deletePlaces(String id) throws ProjectExeption {
+        try {
+            Place deletePlace = getPlace(id);
+            if (deletePlace == null) {
+                throw new ProjectExeption(TypeMessage.NOT_FOUND);
+            }
+            placesList.remove(deletePlace);
+        } catch (Exception e) {
+            throw new ProjectExeption(TypeMessage.NOT_FOUND_FILE);
+        }
+    }
+
+    public void modifyPlace(String id, Place newPlace) throws ProjectExeption {
+        if (!newPlace.getId().equals(id)) {
+            throw new ProjectExeption(TypeMessage.NOT_FOUND);
+        }
+        try {
+            deletePlaces(id);
+            add(newPlace);
+        } catch (Exception e) {
+            throw new ProjectExeption(TypeMessage.NOT_FOUND_FILE);
+        }
+    }
+
+    public Place getPlace(String id) throws ProjectExeption {
+        try {
+            for (Place place : placesList) {
+                if (place.getId().equals(id)) {
+                    return place;
+                }
+            }
+
+        } catch (Exception e) {
+            throw new ProjectExeption(TypeMessage.NOT_FOUND_FILE);
+        }
+        return null;
+    }
 }
